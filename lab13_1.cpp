4cad55ae0b3fa3b3d671e8fa966d8c10c0047b53
panupong660610888 <panupong_wang@cmu.ac.th>
2026-01-09 00:20:26 +0700

    complete

--- lab13_1.cpp
+++ lab13_1.cpp
@@ -19,3 +19,34 @@ int main(){
     cout << "Min = " << B[5];
     return 0;
 }
+
+void stat(const double A[],int N,double B[]){
+    
+    double sum_am = 0;
+    double sum_sd = 0;
+    double sum_gm = 0;
+    double sum_hm = 0;
+    double max = A[0];
+    double min = A[0];
+    
+    for (int i = 0; i < N; i++){
+        sum_am += A[i];
+    }
+    
+     B[0] = sum_am/N;
+     
+     for (int j = 0; j < N; j++){
+         sum_sd += pow(A[j]-B[0],2);
+         sum_gm += log(A[j]);
+         sum_hm += 1/A[j];
+         if (A[j] > max) max = A[j];
+         if (A[j] < min) min = A[j];
+     }
+     
+     B[1] = sqrt(sum_sd/N);
+     B[2] = exp(sum_gm/N);
+     B[3] = N/sum_hm;
+     B[4] = max;
+     B[5] = min;
+     
+}
